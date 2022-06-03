457a26f755c0d87757aabc65c1686fba50e36653
exam01 <darikin123497@mail.ru>
2022-06-03 10:00:28 +0300

    second

--- app/src/main/java/com/example/gg/Activity2.java
+++ app/src/main/java/com/example/gg/Activity2.java
@@ -10,5 +10,6 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        System.out.println("Context");
       }
 }
